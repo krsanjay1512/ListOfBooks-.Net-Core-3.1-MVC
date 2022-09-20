35af85d6cf2aa0d87e69a903e48a5779fc01bab0
Sanjay Kumar <krsanjay1512@gmail.com>
2022-09-20 16:48:30 +0530

    commit

---
+++ BulkyBookWeb/BulkyBookWeb/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBookWeb.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Auther { get; set; }
+        public string ISBN { get; set; }
+    }
+}
